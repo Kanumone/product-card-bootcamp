d129a3d39bbe099fd0ead0a77ea469c4e9de0fee
Kanumone <KuzminAAwork@yandex.ru>
2022-10-16 23:20:10 +0700

    Fix errors

--- js/quantity.js
+++ js/quantity.js
@@ -1,22 +1,19 @@
 const buyButton = document.querySelector('.product-info__button_primary');
 const quantityButtons = document.querySelectorAll('.product-info__quantity button');
 const quantityValue = document.querySelector('.quantity');
-let quantity = Number(quantityValue.textContent);
 const popup = $('.notification');
 
 quantityButtons[0].addEventListener('click', () => {
-  quantity--;
-  quantityValue.textContent = quantity;
-  quantityButtons[0].disabled = quantity === 1;
+  quantityValue.value--;
+  quantityButtons[0].disabled = quantityValue.value <= 1;
 })
 quantityButtons[1].addEventListener('click', () => {
-  quantity++;
-  quantityValue.textContent = quantity;
-  quantityButtons[0].disabled = quantity === 1;
+  quantityValue.value++;
+  quantityButtons[0].disabled = quantityValue.value <= 1;
 })
 
 buyButton.addEventListener('click', () => {
-  popup.text(`В корзину добавлено ${quantity} товаров`);
+  popup.text(`В корзину добавлено ${quantityValue.value} товаров`);
   popup.fadeIn();
   setTimeout(() => {
     popup.fadeOut();
