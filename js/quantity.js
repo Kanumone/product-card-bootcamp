76ab63eba099a280c82ffdd654604e4e54461793
Kanumone <KuzminAAwork@yandex.ru>
2022-10-16 20:54:02 +0700

    Add popup script

--- js/quantity.js
+++ js/quantity.js
@@ -1,6 +1,8 @@
+const buyButton = document.querySelector('.product-info__button_primary');
 const quantityButtons = document.querySelectorAll('.product-info__quantity button');
-const quantityValue = document.querySelector('.product-info__quantity div');
-let quantity = Number(quantityValue.textContent) - 1;
+const quantityValue = document.querySelector('.quantity');
+let quantity = Number(quantityValue.textContent);
+const popup = $('.notification');
 
 quantityButtons[0].addEventListener('click', () => {
   quantity--;
@@ -11,6 +13,14 @@ quantityButtons[1].addEventListener('click', () => {
   quantity++;
   quantityValue.textContent = quantity;
   quantityButtons[0].disabled = quantity === 1;
+})
+
+buyButton.addEventListener('click', () => {
+  popup.text(`В корзину добавлено ${quantity} товаров`);
+  popup.fadeIn();
+  setTimeout(() => {
+    popup.fadeOut();
+  }, 1000);
 })
 
 
